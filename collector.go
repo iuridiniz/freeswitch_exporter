d1cd08eca0449a99b5d9f6e99d00f8939267a919
Iuri Diniz <iuridiniz@gmail.com>
2026-07-24 10:05:37 -0300

    make the code concise

--- collector.go
+++ collector.go
@@ -329,7 +329,7 @@ func (c *Collector) scrape(ch chan<- prometheus.Metric) error {
 
 	if c.channelDurationEnabled {
 		if err = c.channelDurationMetrics(ch); err != nil {
-			level.Warn(c.logger).Log("msg", "channel duration histogram scrape failed", "err", err)
+			return err
 		}
 	}
 
